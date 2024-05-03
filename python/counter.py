f3eec2b37be7cab8bac64550141454d9ccb29f83
Naushin Rahman <naushin.r@gmail.com>
2024-05-02 23:16:05 -0400

    imported os and more features to counter script

--- python/counter.py
+++ python/counter.py
@@ -1,9 +1,23 @@
-import sys 
+# counter.py
 
-counter = 0
+import os
 
-def count():
-    global counter
-    counter+=1
-    return counter
+# Path to the file where the counter will be stored
+counter_file = 'counter.txt'
 
+def get_counter():
+    if not os.path.exists(counter_file):
+        # Create counter file if it doesn't exist
+        with open(counter_file, 'w') as f:
+            f.write('0')
+    with open(counter_file, 'r') as f:
+        return int(f.read())
+
+def increment_counter():
+    counter = get_counter()
+    counter += 1
+    with open(counter_file, 'w') as f:
+        f.write(str(counter))
+
+if __name__ == "__main__":
+    increment_counter()
