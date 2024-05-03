d1f18a34fcf37879398b4dd8d5f99d424ad7a79b
Naushin Rahman <naushin.r@gmail.com>
2024-05-02 23:17:54 -0400

    ammended some lines for clarity

--- python/counter.py
+++ python/counter.py
@@ -1,13 +1,9 @@
-# counter.py
-
 import os
 
-# Path to the file where the counter will be stored
 counter_file = 'counter.txt'
 
 def get_counter():
     if not os.path.exists(counter_file):
-        # Create counter file if it doesn't exist
         with open(counter_file, 'w') as f:
             f.write('0')
     with open(counter_file, 'r') as f:
